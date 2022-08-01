f492e60e4739a59d0ba21d8b1dc63adf9d988d28
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-07-31 20:22:38 -0600

    Initial commit

---
+++ API-JSON-with-URLSession/API_JSON_with_URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_JSON_with_URLSessionApp.swift
+//  API-JSON-with-URLSession
+//
+//  Created by Byron Mejia on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct API_JSON_with_URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API-JSON-with-URLSession/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  API-JSON-with-URLSession
+//
+//  Created by Byron Mejia on 7/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
